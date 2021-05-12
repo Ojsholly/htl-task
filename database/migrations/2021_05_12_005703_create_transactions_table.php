1fee452c8f672e28a5c1ac54bc1be7855fd8e1ce
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2021-05-12 02:08:38 +0100

    Added required migrations and model relationships.

--- database/migrations/2021_05_12_005703_create_transactions_table.php
+++ database/migrations/2021_05_12_005703_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('mobile_number');
+            $table->integer('user_id')->unsigned();
             $table->decimal('amount', 10, 2);
-            $table->rememberToken();
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 }
